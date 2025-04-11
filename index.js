f80a729e1b6f68617a19ed2ded030cff6da710cc
enginalemdar <162575473+enginalemdar@users.noreply.github.com>
2025-04-11 21:31:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ app.use(express.json());
 app.post("/crawl", async (req, res) => {
   const { keyword, webhook } = req.body;
 
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({
+  headless: true,
+  args: ['--no-sandbox', '--disable-setuid-sandbox']
+});
   const page = await browser.newPage();
   await page.goto("https://www.yatirimadestek.gov.tr/arama");
 
